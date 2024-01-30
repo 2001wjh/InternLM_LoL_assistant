7d88520479c14084e2dc1475664c3d26dbf54fb2
Jianheng Wang <145626756+2001wjh@users.noreply.github.com>
2024-01-30 17:11:24 +0800

    Add files via upload

---
+++ download_hf.py
@@ -0,0 +1,7 @@
+import os
+
+# 设置环境变量
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+# 下载模型
+os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /root/lc-lol/model/sentence-transformer')
